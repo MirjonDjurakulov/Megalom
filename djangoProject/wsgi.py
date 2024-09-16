699d956e27511973ed9cbb2aeefd82a491f6ef84
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2024-09-16 20:58:01 +0500

    Edit wsgi.py

--- djangoProject/wsgi.py
+++ djangoProject/wsgi.py
@@ -9,6 +9,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+activate_this = os.path.expanduser('~/site/venv/bin/activate_this.py')
+
+exec(open(activate_this).read(), {'__file__': activate_this})
+
+sys.path.insert(1, os.path.expanduser('~/site/public_html/'))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings.production')
